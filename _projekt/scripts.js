de1ca3a421c7384fe6cd70e19dd84cadf3a48cc0
Michal Rubajczyk <rubajticos@gmail.com>
2020-10-14 22:29:23 +0200

    Add and remove tooltip

--- _projekt/scripts.js
+++ _projekt/scripts.js
@@ -1,6 +1,22 @@
-(function() {
+(function () {
 
+    function showToolTip() {
+        console.log("Show Tooltip");
+    }
 
+    function removeToolTip() {
+        console.log("Remove Tooltip");
+    }
 
+    function registerTooltipListener() {
+        const title = document.querySelectorAll("[title]");
+
+        for (let i = 0; i < title.length; i++) {
+            title[i].addEventListener("mouseenter", showToolTip, false);
+            title[i].addEventListener("mouseleave", removeToolTip, false)
+        }
+    }
+
+    registerTooltipListener()
 
 })();
