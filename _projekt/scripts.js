3f8e8e95844f62a649ee1e85aa61f5c2133fcd82
Michal Rubajczyk <rubajticos@gmail.com>
2020-10-14 22:36:40 +0200

    Create tooltip

--- _projekt/scripts.js
+++ _projekt/scripts.js
@@ -1,13 +1,5 @@
 (function () {
 
-    function showToolTip() {
-        console.log("Show Tooltip");
-    }
-
-    function removeToolTip() {
-        console.log("Remove Tooltip");
-    }
-
     function registerTooltipListener() {
         const title = document.querySelectorAll("[title]");
 
@@ -15,6 +7,24 @@
             title[i].addEventListener("mouseenter", showToolTip, false);
             title[i].addEventListener("mouseleave", removeToolTip, false)
         }
+    }
+
+    function showToolTip(e) {
+        const title = e.target.getAttribute("title");
+        
+        createTooltip(title);
+    }
+
+    function createTooltip(text) {
+        const div = document.createElement("div")
+
+        div.textContent = text;
+        div.className = "tooltip";
+        document.body.appendChild(div);
+    }
+
+    function removeToolTip() {
+        console.log("Remove Tooltip");
     }
 
     registerTooltipListener()
