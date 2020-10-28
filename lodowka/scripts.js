2588ed6e38c052e4b3d00ebb50de558dfed90ecc
Michal Rubajczyk <rubajticos@gmail.com>
2020-10-28 23:25:56 +0100

    Add possibility to add todo list

--- lodowka/scripts.js
+++ lodowka/scripts.js
@@ -4,11 +4,8 @@
         const generalActionButton = document.querySelector("div.action-button.action-button__general.always-visible");
         const actionButtonContainer = document.querySelector("div.action-button__container");
 
-        console.log(generalActionButton);
-
         generalActionButton.addEventListener("mouseover", showActionButtons);
         actionButtonContainer.addEventListener("mouseleave", hideActionButtons);
-
     }
 
     function showActionButtons() {
@@ -29,6 +26,43 @@
         })
     }
 
+    function setAddTodoButton() {
+        const actionButtonContainer = document.querySelector("div.action-button__container");
+        const addTodoCardButton = actionButtonContainer.querySelector("div.action-button__todo");
+
+        addTodoCardButton.addEventListener("click", addTodoCard);
+    }
+
+    function addTodoCard() {
+        const actionButtonContainer = document.querySelector("#content");
+        const todoCard = createTodoCard();
+        todoCard.classList.add('box', 'todo-list');
+
+        actionButtonContainer.appendChild(todoCard);
+        console.log(todoCard);
+    }
+
+    function createTodoCard() {
+        const todoCard = document.createElement("div");
+        todoCard.classList.add('box', 'todo-list');
+
+        const cardTitle = document.createElement("span");
+        cardTitle.classList.add("todo-list__title");
+        cardTitle.innerHTML = "Title"
+
+        const itemsList = document.createElement("ul");
+
+        const newItemButton = document.createElement("button");
+        newItemButton.innerHTML = "+";
+
+        todoCard.appendChild(cardTitle);
+        todoCard.appendChild(itemsList);
+        todoCard.appendChild(newItemButton);
+
+        return todoCard;
+    }
+
     handleActionButtonsDisplaying();
+    setAddTodoButton();
 
 })();
