cf149f3f4efec9a6910913b6bf4a89b1a5bcfda8
Michal Rubajczyk <rubajticos@gmail.com>
2020-10-28 23:03:00 +0100

    Action button container with animations

--- lodowka/scripts.js
+++ lodowka/scripts.js
@@ -1,4 +1,34 @@
-(function () {   
+(function() {
 
+    function handleActionButtonsDisplaying() {
+        const generalActionButton = document.querySelector("div.action-button.action-button__general.always-visible");
+        const actionButtonContainer = document.querySelector("div.action-button__container");
+
+        console.log(generalActionButton);
+
+        generalActionButton.addEventListener("mouseover", showActionButtons);
+        actionButtonContainer.addEventListener("mouseleave", hideActionButtons);
+
+    }
+
+    function showActionButtons() {
+        const actionButtonContainer = document.querySelector("div.action-button__container");
+        const actionButtons = actionButtonContainer.querySelectorAll(".action-button")
+
+        actionButtons.forEach(btn => btn.classList.remove("hidden"));
+    }
+
+    function hideActionButtons() {
+        const actionButtonContainer = document.querySelector("div.action-button__container");
+        const actionButtons = actionButtonContainer.querySelectorAll(".action-button")
+
+        actionButtons.forEach(btn => {
+            if (!btn.classList.contains("always-visible")) {
+                btn.classList.add("hidden");
+            }
+        })
+    }
+
+    handleActionButtonsDisplaying();
 
 })();
