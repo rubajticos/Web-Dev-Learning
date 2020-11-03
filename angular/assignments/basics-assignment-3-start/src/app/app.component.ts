822c582b2d385df25985f93c3b37098a297959c5
Michal Rubajczyk <rubajticos@gmail.com>
2020-11-03 23:06:05 +0100

    Assignment 3

--- angular/assignments/basics-assignment-3-start/src/app/app.component.ts
+++ angular/assignments/basics-assignment-3-start/src/app/app.component.ts
@@ -6,4 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  i = 1;
+  logs = [];
+  displaySecret = false;
+
+  onButtonClick() {
+    this.logs.push(this.i++);
+    this.toggleSecret();
+  }
+
+  toggleSecret() {
+    this.displaySecret = !this.displaySecret;
+  }
+
 }
