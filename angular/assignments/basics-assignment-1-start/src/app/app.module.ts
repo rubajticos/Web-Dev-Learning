ec2e7a8c5523b1bd1e94b392c9b173ab701e0561
Michal Rubajczyk <rubajticos@gmail.com>
2020-11-03 20:27:59 +0100

    Assignmen 1: Practicing Components

--- angular/assignments/basics-assignment-1-start/src/app/app.module.ts
+++ angular/assignments/basics-assignment-1-start/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { WarningAlertComponent } from './warning-alert/warning-alert.component';
+import { SuccessAlertComponent } from './success-alert/success-alert.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    WarningAlertComponent
+    WarningAlertComponent,
+    SuccessAlertComponent
   ],
   imports: [
     BrowserModule,
