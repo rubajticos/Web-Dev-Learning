2354131b38635bb10d1793c632d452801d393dbd
Michal Rubajczyk <rubajticos@gmail.com>
2020-11-05 19:01:50 +0100

    47. Creating the Components

--- angular/project/src/app/app.module.ts
+++ angular/project/src/app/app.module.ts
@@ -4,10 +4,24 @@ import { FormsModule } from '@angular/forms';
 
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './component/header/header.component';
+import { RecipesComponent } from './component/recipes/recipes.component';
+import { RecipeListComponent } from './component/recipes/recipe-list/recipe-list.component';
+import { RecipeDetailComponent } from './component/recipes/recipe-detail/recipe-detail.component';
+import { RecipeItemComponent } from './component/recipes/recipe-list/recipe-item/recipe-item.component';
+import { ShoppingListComponent } from './component/shopping-list/shopping-list.component';
+import { ShoppingEditComponent } from './component/shopping-list/shopping-edit/shopping-edit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    RecipesComponent,
+    RecipeListComponent,
+    RecipeDetailComponent,
+    RecipeItemComponent,
+    ShoppingListComponent,
+    ShoppingEditComponent
   ],
   imports: [
     BrowserModule,
