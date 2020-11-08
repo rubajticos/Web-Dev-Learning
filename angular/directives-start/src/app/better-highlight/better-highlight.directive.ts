58192bce8b38401121a6c2ce32064035a149b535
Michal Rubajczyk <rubajticos@gmail.com>
2020-11-08 20:12:01 +0100

    98. Binding to Directive Properties - alias

--- angular/directives-start/src/app/better-highlight/better-highlight.directive.ts
+++ angular/directives-start/src/app/better-highlight/better-highlight.directive.ts
@@ -5,7 +5,7 @@ import { Directive, ElementRef, HostBinding, HostListener, Input, OnInit, Render
 })
 export class BetterHighlightDirective implements OnInit {
   @Input() defaultColor: string = 'transparent';
-  @Input() highlightColor: string = 'blue'
+  @Input('appBetterHighlight') highlightColor: string = 'blue'
   @HostBinding('style.backgroundColor') backgroundColor: string;
 
 
