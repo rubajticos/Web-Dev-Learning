353aaf9d9b125e981f99fbdfa96908b33edeac40
Michal Rubajczyk <rubajticos@gmail.com>
2020-11-10 19:55:16 +0100

    117. Managing Recipes in a Recipe Service

--- angular/project/src/app/component/recipes/recipe-list/recipe-list.component.ts
+++ angular/project/src/app/component/recipes/recipe-list/recipe-list.component.ts
@@ -1,26 +1,23 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
+import { RecipeService } from '../recipe.service';
 
 @Component({
-  selector: 'app-recipe-list',
-  templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.css']
+    selector: 'app-recipe-list',
+    templateUrl: './recipe-list.component.html',
+    styleUrls: ['./recipe-list.component.css'],
 })
 export class RecipeListComponent implements OnInit {
-  recipes: Recipe[] = [
-    new Recipe('A Test Recipe', 'This is simply a test', 'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg'),
-    new Recipe('Another Test Recipe ', 'This is simply a test 2', 'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg')
-  ];
-  @Output() recipeWasSelected = new EventEmitter<Recipe>();
+    @Output() recipeWasSelected = new EventEmitter<Recipe>();
+    recipes: Recipe[];
 
-  constructor() { }
+    constructor(private recipeService: RecipeService) {}
 
-  ngOnInit(): void {
-    
-  }
+    ngOnInit(): void {
+        this.recipes = this.recipeService.getRecipes();
+    }
 
-  onRecipeSelected(recipe: Recipe) {
-    this.recipeWasSelected.emit(recipe);
-  }
-
+    onRecipeSelected(recipe: Recipe) {
+        this.recipeWasSelected.emit(recipe);
+    }
 }
---
+++ angular/project/src/app/component/recipes/recipe.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { Recipe } from './recipe.model';
+
+@Injectable({
+    providedIn: 'root',
+})
+export class RecipeService {
+    private recipes: Recipe[] = [
+        new Recipe(
+            'A Test Recipe',
+            'This is simply a test',
+            'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg'
+        ),
+        new Recipe(
+            'Another Test Recipe ',
+            'This is simply a test 2',
+            'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg'
+        ),
+    ];
+
+    constructor() {}
+
+    getRecipes() {
+        return this.recipes.slice();
+    }
+}
