1f9aeaef66eb3159424ad96d3f68f153825bf333
Michal Rubajczyk <rubajticos@gmail.com>
2020-11-10 20:56:53 +0100

    122. Passing Ingredients from Recipes to the Shopping List(via a Service)

--- angular/project/src/app/component/recipes/recipe.service.ts
+++ angular/project/src/app/component/recipes/recipe.service.ts
@@ -1,5 +1,6 @@
 import { EventEmitter, Injectable } from '@angular/core';
 import { Ingredient } from 'src/app/shared/ingredient.model';
+import { ShoppingListService } from '../shopping-list/shopping-list.service';
 import { Recipe } from './recipe.model';
 
 @Injectable({
@@ -23,9 +24,13 @@ export class RecipeService {
         ),
     ];
 
-    constructor() {}
+    constructor(private slService: ShoppingListService) {}
 
     getRecipes() {
         return this.recipes.slice();
+    }
+
+    addIngredientsToShoppingList(ingredients: Ingredient[]) {
+        this.slService.addIngredients(ingredients);
     }
 }
