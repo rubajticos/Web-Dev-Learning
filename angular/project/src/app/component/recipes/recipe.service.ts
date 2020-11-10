0816ce091c99125a37816d7a49f0a3a010509be8
Michal Rubajczyk <rubajticos@gmail.com>
2020-11-10 20:45:22 +0100

    121. Adding Ingredients to Recipes

--- angular/project/src/app/component/recipes/recipe.service.ts
+++ angular/project/src/app/component/recipes/recipe.service.ts
@@ -1,4 +1,5 @@
 import { EventEmitter, Injectable } from '@angular/core';
+import { Ingredient } from 'src/app/shared/ingredient.model';
 import { Recipe } from './recipe.model';
 
 @Injectable({
@@ -9,14 +10,16 @@ export class RecipeService {
 
     private recipes: Recipe[] = [
         new Recipe(
-            'A Test Recipe',
-            'This is simply a test',
-            'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg'
+            'Tasty Schnitzel',
+            'A super-tasty Schnitzel - just awesome!',
+            'https://www.thespruceeats.com/thmb/cckc3_4QUQ79kSFhcLPM8xg9F3g=/3797x2848/smart/filters:no_upscale()/wiener-schnitzel-recipe-1447089-Hero-5b587d6c46e0fb0071b0059d.jpg',
+            [new Ingredient('Meat', 1), new Ingredient('French Fries', 20)]
         ),
         new Recipe(
-            'Another Test Recipe ',
-            'This is simply a test 2',
-            'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg'
+            'Big fat Burder',
+            'What else you need to say?',
+            'https://s3.przepisy.pl/przepisy3ii/img/variants/800x0/wege_burger_z_pieczonymi_baklazanami_0237983.jpg',
+            [new Ingredient('Buns', 2), new Ingredient('Meat', 1)]
         ),
     ];
 
