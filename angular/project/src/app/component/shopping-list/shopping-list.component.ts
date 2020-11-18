660717e0f7623a0b282e6e4f84fd17a8a0444d90
Michal Rubajczyk <rubajticos@gmail.com>
2020-11-18 21:18:04 +0100

    179. Improving the Reactive Service with Observables(Subjects)

--- angular/project/src/app/component/shopping-list/shopping-list.component.ts
+++ angular/project/src/app/component/shopping-list/shopping-list.component.ts
@@ -1,4 +1,11 @@
-import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import {
+    Component,
+    ElementRef,
+    OnDestroy,
+    OnInit,
+    ViewChild,
+} from '@angular/core';
+import { Subscription } from 'rxjs';
 import { Ingredient } from 'src/app/shared/ingredient.model';
 import { ShoppingListService } from './shopping-list.service';
 
@@ -7,17 +14,22 @@ import { ShoppingListService } from './shopping-list.service';
     templateUrl: './shopping-list.component.html',
     styleUrls: ['./shopping-list.component.css'],
 })
-export class ShoppingListComponent implements OnInit {
+export class ShoppingListComponent implements OnInit, OnDestroy {
     ingredients: Ingredient[];
+    private igChangeSub: Subscription;
 
     constructor(private slService: ShoppingListService) {}
 
     ngOnInit(): void {
         this.ingredients = this.slService.getIngredients();
-        this.slService.ingredientsChanged.subscribe(
+        this.igChangeSub = this.slService.ingredientsChanged.subscribe(
             (ingredients: Ingredient[]) => {
                 this.ingredients = ingredients;
             }
         );
+    }
+
+    ngOnDestroy(): void {
+        this.igChangeSub.unsubscribe();
     }
 }
--- angular/project/src/app/component/shopping-list/shopping-list.service.ts
+++ angular/project/src/app/component/shopping-list/shopping-list.service.ts
@@ -1,11 +1,12 @@
-import { EventEmitter, Injectable, Output } from '@angular/core';
+import { Injectable, Output } from '@angular/core';
+import { Subject } from 'rxjs';
 import { Ingredient } from 'src/app/shared/ingredient.model';
 
 @Injectable({
     providedIn: 'root',
 })
 export class ShoppingListService {
-    @Output() ingredientsChanged = new EventEmitter<Ingredient[]>();
+    @Output() ingredientsChanged = new Subject<Ingredient[]>();
 
     private ingredients: Ingredient[] = [
         new Ingredient('Apples', 5),
@@ -20,7 +21,7 @@ export class ShoppingListService {
 
     addIngredient(ingredient: Ingredient) {
         this.ingredients.push(ingredient);
-        this.ingredientsChanged.emit(this.ingredients.slice());
+        this.ingredientsChanged.next(this.ingredients.slice());
     }
 
     addIngredients(ingredients: Ingredient[]) {
@@ -28,6 +29,6 @@ export class ShoppingListService {
         //     this.addIngredient(ingredient);
         // }
         this.ingredients.push(...ingredients);
-        this.ingredientsChanged.emit(this.ingredients.slice());
+        this.ingredientsChanged.next(this.ingredients.slice());
     }
 }
