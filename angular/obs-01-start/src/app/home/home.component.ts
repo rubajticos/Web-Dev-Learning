6e551d8d8497fd65798d6a4a2a466b967ddeb905
Michal Rubajczyk <rubajticos@gmail.com>
2020-11-18 20:02:34 +0100

    173. Errors & Completion

--- angular/obs-01-start/src/app/home/home.component.ts
+++ angular/obs-01-start/src/app/home/home.component.ts
@@ -19,6 +19,12 @@ export class HomeComponent implements OnInit, OnDestroy {
             let count = 0;
             setInterval(() => {
                 observer.next(count);
+                if (count == 2) {
+                    observer.complete();
+                }
+                if (count > 3) {
+                    observer.error(new Error('Count is greater 3!'));
+                }
                 count++;
             }, 1000);
         });
@@ -26,6 +32,13 @@ export class HomeComponent implements OnInit, OnDestroy {
         this.firstObsSubscription = customIntervalObservable.subscribe(
             (data) => {
                 console.log(data);
+            },
+            (error) => {
+                console.log(error);
+                alert(error.message);
+            },
+            () => {
+                console.log('Completed');
             }
         );
     }
