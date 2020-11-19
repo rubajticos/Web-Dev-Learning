6d7c52207630ea4eb3298f2a32f1cf529cf0875c
Michal Rubajczyk <rubajticos@gmail.com>
2020-11-19 21:30:14 +0100

    194. TD: Using ngModel with Two-Way-Binding

--- angular/forms-td-start/src/app/app.component.ts
+++ angular/forms-td-start/src/app/app.component.ts
@@ -9,6 +9,7 @@ import { NgForm } from '@angular/forms';
 export class AppComponent {
     @ViewChild('f', { static: true }) signupForm: NgForm;
     defaultQuestion = 'pet';
+    answer = '';
 
     suggestUserName() {
         const suggestedName = 'Superuser';
