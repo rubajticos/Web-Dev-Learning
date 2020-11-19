7175b0dca48b20d0138d19afd024676c86ee6aaa
Michal Rubajczyk <rubajticos@gmail.com>
2020-11-19 22:36:41 +0100

    Assignment 6 - my solution

--- angular/assignments/forms-td-assignment-start/src/app/app.component.ts
+++ angular/assignments/forms-td-assignment-start/src/app/app.component.ts
@@ -1,8 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.css'],
 })
-export class AppComponent {}
+export class AppComponent {
+    @ViewChild('f', { static: true }) myForm;
+    defaultSubsription = 'advanced';
+
+    onSubmit() {
+        console.log(this.myForm.value);
+    }
+}
