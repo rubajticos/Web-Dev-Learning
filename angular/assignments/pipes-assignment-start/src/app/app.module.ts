49f623b71c381515b4a425489850053760dad29c
Michal Rubajczyk <rubajticos@gmail.com>
2020-11-25 23:16:27 +0100

    Assignment 8

--- angular/assignments/pipes-assignment-start/src/app/app.module.ts
+++ angular/assignments/pipes-assignment-start/src/app/app.module.ts
@@ -3,9 +3,11 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { SortPipe } from './sort.pipe';
+import { ReversePipe } from './reverse.pipe';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, SortPipe, ReversePipe],
   imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
