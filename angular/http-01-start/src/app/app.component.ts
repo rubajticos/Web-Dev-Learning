b1a27eb3ba448e848b99d5dec211b66e960aa746
mrubajcz <mrubajcz@softsystem.pl>
2020-11-26 13:11:10 +0100

    249-253 - init

---
+++ angular/http-01-start/src/app/app.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent implements OnInit {
+  loadedPosts = [];
+
+  constructor() {}
+
+  ngOnInit() {}
+
+  onCreatePost(postData: { title: string; content: string }) {
+    // Send Http request
+    console.log(postData);
+  }
+
+  onFetchPosts() {
+    // Send Http request
+  }
+
+  onClearPosts() {
+    // Send Http request
+  }
+}
