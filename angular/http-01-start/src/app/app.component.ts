0106785b34c3c211d16801b6b181b7e4701a7e9f
Michal Rubajczyk <rubajticos@gmail.com>
2020-11-26 21:10:43 +0100

    258. Outputting Posts

--- angular/http-01-start/src/app/app.component.ts
+++ angular/http-01-start/src/app/app.component.ts
@@ -9,7 +9,7 @@ import { Post } from './post.model';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent implements OnInit {
-  loadedPosts = [];
+  loadedPosts: Post[] = [];
 
   constructor(private http: HttpClient) {}
 
@@ -50,6 +50,7 @@ export class AppComponent implements OnInit {
         })
       )
       .subscribe((posts) => {
+        this.loadedPosts = posts;
         console.log(posts);
       });
   }
