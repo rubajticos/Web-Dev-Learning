e2cdf9d9e9f37997baf064e43088fe0cb3445bf3
Michal Rubajczyk <rubajticos@gmail.com>
2020-11-26 20:27:17 +0100

    255. GETting Data

--- angular/http-01-start/src/app/app.component.ts
+++ angular/http-01-start/src/app/app.component.ts
@@ -11,7 +11,9 @@ export class AppComponent implements OnInit {
 
   constructor(private http: HttpClient) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.fetchPosts();
+  }
 
   onCreatePost(postData: { title: string; content: string }) {
     // Send Http request
@@ -22,10 +24,16 @@ export class AppComponent implements OnInit {
   }
 
   onFetchPosts() {
-    // Send Http request
+    this.fetchPosts();
   }
 
   onClearPosts() {
     // Send Http request
+  }
+
+  fetchPosts() {
+    this.http.get('https://ng-complete-guide-d6c42.firebaseio.com/posts.json').subscribe((posts) => {
+      console.log(posts);
+    });
   }
 }
