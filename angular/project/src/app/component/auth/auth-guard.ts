c0efdd250de3a534fbe122f6a037a294fc4a9520
Michal Rubajczyk <rubajticos@gmail.com>
2020-12-01 22:25:12 +0100

    305. Adding an Auth Guard - UrlTree and take user only once

--- angular/project/src/app/component/auth/auth-guard.ts
+++ angular/project/src/app/component/auth/auth-guard.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
-import { map, tap } from 'rxjs/operators';
+import { map, take, tap } from 'rxjs/operators';
 import { AuthService } from './auth.service';
 
 @Injectable({ providedIn: 'root' })
@@ -11,16 +11,22 @@ export class AuthGuard implements CanActivate {
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot
-  ): boolean | Promise<boolean> | Observable<boolean> {
+  ): boolean | UrlTree | Promise<boolean | UrlTree> | Observable<boolean | UrlTree> {
     return this.authService.user.pipe(
+      take(1),
       map((user) => {
-        return !!user;
-      }),
-      tap((isAuth) => {
-        if (!isAuth) {
-          this.router.navigate(['/auth']);
+        const isAuth = !!user;
+        if (isAuth) {
+          return true;
         }
+
+        return this.router.createUrlTree(['/auth']);
       })
+      //   tap((isAuth) => {
+      //     if (!isAuth) {
+      //       this.router.navigate(['/auth']);
+      //     }
+      //   })
     );
   }
 }
