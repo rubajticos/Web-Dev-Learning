ff828f1ac9fb8b772d64d14be4d429f39bc6f718
Michal Rubajczyk <rubajticos@gmail.com>
2020-12-03 21:58:57 +0100

    321. Getting Started with Feature Modules

--- angular/project/src/app/app.module.ts
+++ angular/project/src/app/app.module.ts
@@ -4,42 +4,31 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './component/header/header.component';
-import { RecipesComponent } from './component/recipes/recipes.component';
-import { RecipeListComponent } from './component/recipes/recipe-list/recipe-list.component';
-import { RecipeDetailComponent } from './component/recipes/recipe-detail/recipe-detail.component';
-import { RecipeItemComponent } from './component/recipes/recipe-list/recipe-item/recipe-item.component';
 import { ShoppingListComponent } from './component/shopping-list/shopping-list.component';
 import { ShoppingEditComponent } from './component/shopping-list/shopping-edit/shopping-edit.component';
 import { DropdownDirective } from './shared/dropdown.directive';
 import { AppRoutingModule } from './app-routing.module';
-import { RecipeStartComponent } from './component/recipes/recipe-start/recipe-start.component';
-import { RecipeEditComponent } from './component/recipes/recipe-edit/recipe-edit.component';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { AuthComponent } from './component/auth/auth.component';
 import { LoadingSpinnerComponent } from './shared/loading-spinner/loading-spinner.component';
 import { AuthInterceptorService } from './component/auth/auth-interceptor.service';
 import { AlertComponent } from './shared/alert/alert.component';
 import { PlaceholderDirective } from './shared/placeholder/placeholder.directive';
+import { RecipesModule } from './component/recipes/recipes.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    RecipesComponent,
-    RecipeListComponent,
-    RecipeDetailComponent,
-    RecipeItemComponent,
     ShoppingListComponent,
     ShoppingEditComponent,
     DropdownDirective,
-    RecipeStartComponent,
-    RecipeEditComponent,
     AuthComponent,
     LoadingSpinnerComponent,
     AlertComponent,
     PlaceholderDirective,
   ],
-  imports: [BrowserModule, FormsModule, ReactiveFormsModule, AppRoutingModule, HttpClientModule],
+  imports: [BrowserModule, FormsModule, ReactiveFormsModule, AppRoutingModule, HttpClientModule, RecipesModule],
   providers: [{ provide: HTTP_INTERCEPTORS, useClass: AuthInterceptorService, multi: true }],
   bootstrap: [AppComponent],
   entryComponents: [AlertComponent],
