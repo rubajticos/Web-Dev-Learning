16c3a87248d82b1598e84739cff6245f09147cbf
Michal Rubajczyk <rubajticos@gmail.com>
2020-12-03 22:16:02 +0100

    323. Adding Routes to Feature Modules

--- angular/project/src/app/app-routing.module.ts
+++ angular/project/src/app/app-routing.module.ts
@@ -1,27 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AuthGuard } from './component/auth/auth-guard';
 import { AuthComponent } from './component/auth/auth.component';
-import { RecipeDetailComponent } from './component/recipes/recipe-detail/recipe-detail.component';
-import { RecipeEditComponent } from './component/recipes/recipe-edit/recipe-edit.component';
-import { RecipeStartComponent } from './component/recipes/recipe-start/recipe-start.component';
-import { RecipesResolverService } from './component/recipes/recipes-resolver.service';
-import { RecipesComponent } from './component/recipes/recipes.component';
 import { ShoppingListComponent } from './component/shopping-list/shopping-list.component';
 
 const appRoutes: Routes = [
   { path: '', redirectTo: '/recipes', pathMatch: 'full' },
-  {
-    path: 'recipes',
-    component: RecipesComponent,
-    canActivate: [AuthGuard],
-    children: [
-      { path: '', component: RecipeStartComponent },
-      { path: 'new', component: RecipeEditComponent },
-      { path: ':id', component: RecipeDetailComponent, resolve: [RecipesResolverService] },
-      { path: ':id/edit', component: RecipeEditComponent },
-    ],
-  },
   { path: 'shopping-list', component: ShoppingListComponent },
   { path: 'auth', component: AuthComponent },
 ];
