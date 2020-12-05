a2f22657a249449a61efdaddac1712163dba8488
Michal Rubajczyk <rubajticos@gmail.com>
2020-12-05 16:01:31 +0100

    334. Loading Services Differently - two instances example

--- angular/project/src/app/component/shopping-list/shopping-list.module.ts
+++ angular/project/src/app/component/shopping-list/shopping-list.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { SharedModule } from 'src/app/shared/shared.module';
+import { LoggingService } from '../logging.service';
 import { ShoppingEditComponent } from './shopping-edit/shopping-edit.component';
 import { ShoppingListComponent } from './shopping-list.component';
 
@@ -9,5 +10,6 @@ import { ShoppingListComponent } from './shopping-list.component';
   declarations: [ShoppingListComponent, ShoppingEditComponent],
   imports: [SharedModule, FormsModule, RouterModule.forChild([{ path: '', component: ShoppingListComponent }])],
   exports: [ShoppingListComponent, ShoppingEditComponent],
+  providers: [LoggingService],
 })
 export class ShoppingListModule {}
