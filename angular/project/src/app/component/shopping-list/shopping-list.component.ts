270086e05edbbdc9dd70661b2c119bdcb175a91f
Michal Rubajczyk <rubajticos@gmail.com>
2020-12-05 15:54:07 +0100

    334. Loading Services Differently - provided in root

--- angular/project/src/app/component/shopping-list/shopping-list.component.ts
+++ angular/project/src/app/component/shopping-list/shopping-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, ElementRef, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { Subscription } from 'rxjs';
 import { Ingredient } from 'src/app/shared/ingredient.model';
+import { LoggingService } from '../logging.service';
 import { ShoppingListService } from './shopping-list.service';
 
 @Component({
@@ -12,13 +13,14 @@ export class ShoppingListComponent implements OnInit, OnDestroy {
   ingredients: Ingredient[];
   private subscription: Subscription;
 
-  constructor(private slService: ShoppingListService) {}
+  constructor(private slService: ShoppingListService, private loggingService: LoggingService) {}
 
   ngOnInit(): void {
     this.ingredients = this.slService.getIngredients();
     this.subscription = this.slService.ingredientsChanged.subscribe((ingredients: Ingredient[]) => {
       this.ingredients = ingredients;
     });
+    this.loggingService.printLog('Hello from ShoppingListComponent ngOnInit!');
   }
 
   ngOnDestroy(): void {
