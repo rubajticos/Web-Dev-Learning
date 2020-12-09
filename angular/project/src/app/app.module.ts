02e0e266c15500fef012dd086f3289183f9a27f2
Michal Rubajczyk <rubajticos@gmail.com>
2020-12-09 22:58:06 +0100

    349. Setting Up the NgRxStore

--- angular/project/src/app/app.module.ts
+++ angular/project/src/app/app.module.ts
@@ -9,10 +9,19 @@ import { AlertComponent } from './shared/alert/alert.component';
 import { SharedModule } from './shared/shared.module';
 import { CoreModule } from './component/core.module';
 import { LoggingService } from './component/logging.service';
+import { StoreModule } from '@ngrx/store';
+import { shoppingListReducer } from './component/shopping-list/store/shopping-list.reducer';
 
 @NgModule({
   declarations: [AppComponent, HeaderComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule, SharedModule, CoreModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    StoreModule.forRoot({ shoppingList: shoppingListReducer }),
+    SharedModule,
+    CoreModule,
+  ],
   bootstrap: [AppComponent],
   entryComponents: [AlertComponent],
   // providers: [LoggingService],
