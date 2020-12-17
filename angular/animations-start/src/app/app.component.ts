f090ce3000569cb5981582f7a2f5fe959b492a85
Michal Rubajczyk <rubajticos@gmail.com>
2020-12-17 12:26:57 +0100

    410. The "void" State

--- angular/animations-start/src/app/app.component.ts
+++ angular/animations-start/src/app/app.component.ts
@@ -60,6 +60,31 @@ import { Component } from '@angular/core';
         animate(500),
       ]),
     ]),
+    trigger('list1', [
+      state(
+        'in',
+        style({
+          opacity: 1,
+          transform: 'translateX(0)',
+        })
+      ),
+      transition('void => *', [
+        style({
+          opacity: 0,
+          transform: 'translateX(-100px)',
+        }),
+        animate(300),
+      ]),
+      transition('* => void', [
+        animate(
+          300,
+          style({
+            transform: 'translate(100px)',
+            opacity: 0,
+          })
+        ),
+      ]),
+    ]),
   ],
 })
 export class AppComponent {
@@ -78,5 +103,9 @@ export class AppComponent {
 
   onShrink() {
     this.wildState = 'shrunken';
+  }
+
+  onDelete(item) {
+    this.list.splice(this.list.indexOf(item), 1);
   }
 }
