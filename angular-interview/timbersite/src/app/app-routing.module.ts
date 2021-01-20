91500892bf05e170c0c06da5a08d0015f587782d
Michal Rubajczyk <rubajticos@gmail.com>
2021-01-20 21:59:25 +0100

    Create new Timbers component and configure routing

--- angular-interview/timbersite/src/app/app-routing.module.ts
+++ angular-interview/timbersite/src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
+import { TimbersComponent } from './timbers/timbers.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: AppComponent },
+  { path: 'timbers', component: TimbersComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
---
+++ angular-interview/timbersite/src/app/timbers/timbers.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-timbers',
+  templateUrl: './timbers.component.html',
+  styleUrls: ['./timbers.component.scss']
+})
+export class TimbersComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
